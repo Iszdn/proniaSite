21b1eb082637eba91a99c4157a4bae38cd3f4a38
Orxan-Abdullazada <tu7pnc7lt@code.edu.az>
2023-11-04 13:50:32 +0400

    Pronia site Update

--- scripy.js
+++ scripy.js
@@ -1,18 +1,37 @@
 const searchLoop = document.getElementById("searchLoop");
-const body = document.querySelector("body");
-const modal = document.createElement("div");
-let close = document.createElement("span");
-let searchContent = document.createElement("div");
-let spanSearch = document.createElement("span");
-let inputSearch = document.createElement("input");
-
-close.classList.add("close");
-searchContent.classList.add("searchContent");
-
-searchContent.append(spanSearch, inputSearch);
-modal.append(close, searchContent);
-body.append(modal);
-// console.log(searchLoop);
-searchLoop.addEventListener("click", (e) => {
-  e.preventDefault();
+const searchLoopMedia = document.getElementById("searchLoopMedia");
+const modal=document.getElementById("modal")
+const closeIconForClose=document.getElementById("closeIconForClose")
+const p=document.getElementById("closeP")
+searchLoop.addEventListener("click", (e)=>{
+    console.log("kliklendim");
+    e.preventDefault()
+  modal.classList.add("modalVid")
 });
+closeIconForClose.addEventListener('mouseover',(e)=>{
+    e.preventDefault()
+    closeIconForClose.classList.add("closeIconForClose")
+    // p.classList.add("closeVid")
+})
+closeIconForClose.addEventListener('click',(e)=>{
+    e.preventDefault()
+   modal.classList.remove("modalVid")
+})
+searchLoopMedia.addEventListener("click", (e)=>{
+    console.log("kliklendim");
+    e.preventDefault()
+  modal.classList.add("modalVid")
+});
+// modal end 
+
+
+
+
+// imgSlider 
+const body=document.querySelector('body')
+const ImgSlider=document.getElementById('ImgSlider')
+body.addEventListener('mousemove',(e)=>{
+    let x=Math.floor(e.clientX/100)
+    let y=Math.floor(e.clientY/100)
+    ImgSlider.style.transform=`translate(${-x}px,${-y}px)`
+})
